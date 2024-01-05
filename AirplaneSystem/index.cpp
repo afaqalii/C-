fb431cac979e2afece7e8e87ddb319ce789ced37
afaqalii <afaqali4244@gmail.com>
2024-01-05 09:06:15 +0500

    done with voting system - 2024-01-05 09:06:05

---
+++ AirplaneSystem/index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    const int numOfRows = 13;
+    const int numOfCols = 6;
+    int ticketTypeOption;
+    int rows[numOfRows];
+    int col[numOfCols];
+    cout << "Ticket type \n1: First class  \n2: Business class \n3: Economy class";
+    cin >> ticketTypeOption;
+    if (ticketTypeOption == 1)
+    {
+    }
+    if (ticketTypeOption == 2)
+    {
+    }
+    if (ticketTypeOption == 3)
+    {
+    }
+    return 0;
+}
